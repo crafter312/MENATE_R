128e21f633ea1565adfa72549bd05a59f98885d3
L Webbs <>
2025-06-16 15:41:39 -0500

    added G4VPhysicsConstructor implementation for use in G4VModularPhysicsList

---
+++ MENATE_R.cc
@@ -0,0 +1,100 @@
+/* 
+ * Author : Henry Webb (h.s.webb@wustl.edu)
+ * Date : 16 June 2025
+ * 
+ * Simple G4VPhysicsConstructor implementation of MENATE_R
+ * for integration into a G4VModularPhysicsList. This
+ * implementation using Geant4 advanced example GORAD as
+ * reference, along with the MargotPhysicsList from Brian Roeder.
+ */
+
+#include "MENATE_R.hh"
+
+#include "G4ParticleDefinition.hh"
+#include "G4ParticleTypes.hh"
+#include "G4ProcessManager.hh"
+
+// Particle constructors
+#include "G4BaryonConstructor.hh"
+#include "G4IonConstructor.hh"
+#include "G4LeptonConstructor.hh"
+#include "G4MesonConstructor.hh"
+
+// Light ion physics
+#include "G4eMultipleScattering.hh"
+#include "G4hIonisation.hh"
+
+// Factory
+#include "G4PhysicsConstructorFactory.hh"
+G4_DECLARE_PHYSCONSTR_FACTORY(MENATE_R);
+
+MENATE_R::MENATE_R(const G4String& name) : G4VPhysicsConstructor(name) {}
+
+MENATE_R::~MENATE_R() {}
+
+void MENATE_R::MENATE_R() {}
+
+void MENATE_R::ConstructParticle() {
+	G4Geantino::GeantinoDefinition();
+
+	// Particles
+	G4Gamma::GammaDefinition();
+
+	G4LeptonConstructor pLeptons;
+	pLeptons.ConstructParticle();
+
+	G4BaryonConstructor pBaryons;
+	pBaryons.ConstructParticle();
+
+	G4MesonConstructor pMesons;
+  pMesons.ConstructParticle();
+
+	// Construct Generic Ions
+
+	// Construct all light nuclei!
+	G4Deuteron::DeuteronDefinition();
+	G4Triton::TritonDefinition();
+	G4Alpha::AlphaDefinition();
+	G4He3::He3Definition();
+
+	G4IonConstructor pIons;
+	pIons.ConstructParticle();
+}
+
+void GRParallelWorldPhysics::ConstructProcess() {
+
+	// Add neutron physics
+	G4ProcessManager *pManager = G4Neutron::Neutron()->GetProcessManager();
+	G4String theProcessName = "menate_neutron";
+	menate_R* theMENATE = new menate_R(theProcessName);
+	pManager->AddDiscreteProcess(theMENATE);
+
+	// Light ion physics (EM Stopping Power only)
+	pManager = G4Proton::Proton()->GetProcessManager();
+	pManager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
+	pManager->AddProcess(new G4hIonisation,         -1, 2, 2);
+
+	pManager = G4Deuteron::Deuteron()->GetProcessManager();
+	pManager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
+	pManager->AddProcess(new G4ionIonisation,       -1, 2, 2);
+
+	pManager = G4Triton::Triton()->GetProcessManager();
+	pManager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
+	pManager->AddProcess(new G4ionIonisation,       -1, 2, 2);
+
+	pManager = G4He3::He3()->GetProcessManager();
+	pManager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
+	pManager->AddProcess(new G4ionIonisation,       -1, 2, 2);
+
+	pManager = G4Alpha::Alpha()->GetProcessManager();
+	pManager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
+	pManager->AddProcess(new G4ionIonisation,       -1, 2, 2);
+       	 
+	// Generic ion physics (EM Stopping Power only)
+	pManager = G4GenericIon::GenericIon()->GetProcessManager();
+	pManager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
+	pManager->AddProcess(new G4ionIonisation,       -1, 2, 2);
+}
+
+
+
